982bed75cce593ce5bfa3d1436127632abedd224
Kevin_Yu <Kevin_Yu@adata.com>
2020-08-05 11:41:00 +0800

    update nuget package

--- NavyBule.Core/Messages/Tokenizer.cs
+++ NavyBule.Core/Messages/Tokenizer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Dynamic;
+using System.Linq.Dynamic.Core;
 using System.Net;
 using System.Text.RegularExpressions;
 
